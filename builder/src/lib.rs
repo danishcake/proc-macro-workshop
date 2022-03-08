bda53b6883175d5fb8d66c1087c8f1982722db2d
Edward Woolhouse <danishcake@hotmail.com>
2022-03-08 07:27:03 +0000

    Add code generation required to pass test 2
    * Use the syn crate to parse the input tokens and extract the attributed
      struct name
    * Use the quote crate to generate a TokenStream with the CommandBuilder
      class, the name of which is generated from the attributed struct

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,38 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+
+    // Construct an Ident with the name of the CommandBuilder
+    // The quote macro suppors quoting these as
+    // #ident_name
+    let command_builder_name = format_ident!("{}Builder", input.ident);
+
+    // quote! allows us to write code, and have it transformed into a TokenStream
+    // (albeit a proc_macro2::TokenStream, hence the .into())
+    let output = quote! {
+        pub struct #command_builder_name {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
 
 
-    TokenStream::new()
+        impl Command {
+            pub fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    output.into()
 }
