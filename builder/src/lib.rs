fd3fb639dbf3fad5a3d1c759657d3570f8061905
Edward Woolhouse <danishcake@hotmail.com>
2022-03-08 08:26:27 +0000

    Add setter generation
    * DRY'd up the field generation
    * Added setter generation. This is fairly similar to previous changes, but
      when interpolating it was important to not add commas between entries.
      I found that cargo expand truncates output at the first error

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, DeriveInput, Data, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -12,51 +12,44 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let command_builder_name = format_ident!("{}Builder", input.ident);
 
     // Generate the list of optional fields
-    let field_options = match input.data {
-        Data::Struct(ref data) => {
-            match data.fields {
-                Fields::Named(ref fields) => fields.named.iter().map(|f| {
-                    let field_name = &f.ident;
-                    let field_type = &f.ty;
-
-                    quote! {
-                        #field_name: Option<#field_type>
-                    }
-                }),
-                _ => {
-                    // We don't support tuple structs or tuple variants
-                    unimplemented!()
-                }
-            }
+    let named_fields = match input.data {
+        Data::Struct(ref data) => match data.fields {
+            Fields::Named(ref fields) => &fields.named,
+            _ => unimplemented!(),
         },
-        _ => {
-            // We don't support unions or enums
-            unimplemented!()
-        }
+        _ => unimplemented!(),
     };
+
+    let field_options = named_fields.iter().map(|f| {
+        let field_name = &f.ident;
+        let field_type = &f.ty;
+
+        quote! {
+            #field_name: Option<#field_type>
+        }
+    });
 
     // Generate the list of field initialisers
-    let field_initialisers = match input.data {
-        Data::Struct(ref data) => {
-            match data.fields {
-                Fields::Named(ref fields) => fields.named.iter().map(|f| {
-                    let field_name = &f.ident;
+    let field_initialisers = named_fields.iter().map(|f| {
+        let field_name = &f.ident;
 
-                    quote! {
-                        #field_name: None
-                    }
-                }),
-                _ => {
-                    // We don't support tuple structs or tuple variants
-                    unimplemented!()
-                }
-            }
-        },
-        _ => {
-            // We don't support unions or enums
-            unimplemented!()
+        quote! {
+            #field_name: None
         }
-    };
+    });
+
+    // Generate the list of setters
+    let setters = named_fields.iter().map(|f| {
+        let field_name = &f.ident;
+        let field_type = &f.ty;
+
+        quote! {
+            fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
+                self.#field_name = Some(#field_name);
+                self
+            }
+        }
+    });
 
     // quote! allows us to write code, and have it transformed into a TokenStream
     // (albeit a proc_macro2::TokenStream, hence the .into())
@@ -72,6 +65,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     #(#field_initialisers),*
                 }
             }
+        }
+
+        impl #command_builder_name {
+            #(#setters)*
         }
     };
 
